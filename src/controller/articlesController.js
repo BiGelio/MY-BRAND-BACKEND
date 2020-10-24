46c6b42da393c7813a14dce70a509223e06f535a
Gelio BIZIMANA <geliobizimana01@gmail.com>
2020-10-24 18:08:39 +0200

    feat(articles): adding validation on articles
    -Token verification on articles routes
    -Validations on update article
    
    [finished]

--- src/controller/articlesController.js
+++ src/controller/articlesController.js
@@ -1,50 +1,93 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import articleModel from "../models/articlesModel.js";
-/* Controller get article request*/
+import checkInputs from "../validations/articleValidation.js"
 const articlesModel = mongoose.model("articles");
 export const getArticle = async(req, res) => {
     const data = await articlesModel.find((err, docs) => {
-        if (err) throw new err;
-        console.log(docs);
-        res.send(docs);
+        if (err) {
+            return res.status(500).json({ Message: "Error occurred on server!" })
+        }
+        res.status(200).send(docs);
     })
 }
 
 
 /* Controller post article request*/
 export const postArticle = async(req, res) => {
-    var output = req.body;
-    
+
+    jwt.verify(
+        req.token,
+        process.env.ADMIN_ACCESS_TOKEN, (err) => {
+            if (err) {
+                return res.status(401).json({ Message: "Please login as admin to create new article!" })
+            }
+        }
+    )
+    const valid = checkInputs.validate(req.body);
+    if (valid.error) {
+        return res.status(400).json({ Message: valid.error.details[0].message });
+    }
+    const { title, description } = req.body;
     const posts = new articleModel({
-        title: output.title,
-        description: output.description,
-        date: Date(Date.now())
+        title: title,
+        description: description,
+        createdAt: Date(Date.now())
     });
-    var savePost = await posts.save().then(() => {
-        res.send(`Title:${output.title}\n Description:${output.description}\n date:${Date(Date.now())}`);
-        console.log(output.title + "\n " + output.description + "\n " + Date(Date.now()));
-        console.log("saved to db!")
+    var savePost = await posts.save().then((err) => {
+        if (err) {
+            return res.status(500).json({
+                Message: "Failed to create article,try again!"
+            })
+        }
+        res.status(201).json({
+            Message: "Article created successfully!"
+        })
     })
 }
 
 /* Controller update article request*/
 export const updateArticle = (req, res) => {
-    const data = articlesModel.findByIdAndUpdate({ _id: req.params.id }, { title: "your songs", description: "That album is house of your songs!", date: new Date() }, (err) => {
-        if (!err) {
-            res.status(200).send("Article updated successfully!");
+    jwt.verify(
+        req.token,
+        process.env.ADMIN_ACCESS_TOKEN, (err) => {
+            if (err) {
+                return res.status(401).json({ Message: "Please login as admin to update article!" })
+            }
         }
-
+    )
+    const valid = checkInputs.validate(req.body);
+    if (valid.error) {
+        return res.status(400).json({ Message: valid.error.details[0].message });
+    }
+    const data = articlesModel.findByIdAndUpdate({ _id: req.params.id }, { title: req.body.title, description: req.body.title.description, updatedAt: new Date() }, (err) => {
+        if (err) {
+            return res.status(500).send("Error occurred n server,try again!");
+        }
+        res.status(200).send("Article updated successfully!");
     })
 }
 
 /* Controller delete article request*/
 export const deleteArticle = (req, res) => {
-    articlesModel.remove({ _id: req.params.id }, (err) => {
-        if (!err) {
-            res.status(200).send("Article deleted successfully!");
-        } else {
-            console.log(err);
+    jwt.verify(
+        req.token,
+        process.env.ADMIN_ACCESS_TOKEN, (err) => {
+            if (err) {
+                return res.status(401).json({ Message: "Please login as admin to delete article!" })
+            }
         }
+    )
+    articlesModel.remove({ _id: req.params.id }, (err) => {
+        if (err) {
+            return res.status(200).json({
+                Message: "Deletion failed, try again!"
+            });
+        }
+        res.status(200).json({
+            Message: "Article deleted successfully!"
+        });
+
     })
 
 }
---
+++ src/validations/articleValidation.js
@@ -0,0 +1,5 @@
+import Joi from "joi"
+export const validateArticle = Joi.object().keys({
+    title: Joi.string().min(3).max(40).required(),
+    description: Joi.string().min(2).max(40).required()
+});
